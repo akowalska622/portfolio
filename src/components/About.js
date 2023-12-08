413e125c18acbc7434c739bbdbd752fb82f2461d
Ania Kowalska <63072419+akowalska622@users.noreply.github.com>
2023-12-08 13:50:46 +0000

    tweak about

--- src/components/About.js
+++ src/components/About.js
@@ -11,11 +11,9 @@ export const About = () => {
         alt="Author of the portfolio"
       />
       <p className={classes.description}>
-        Hi there! I'm self-taught web-developer based in Poland, although I
-        consider myself as a world's citizen. I'm deeply passionate about web
+        Hi there! I'm self-taught web-developer from Poland, but currently based in Spain. I'm deeply passionate about web and mobile
         development and I'm eager to share this passion with you! <br />
-        When I don't code, I read books, travel, play with my dog or learn
-        languages.
+        When I don't code, I climb, travel, play with my dog or practice BJJ.
         <br />I can communicate with you in Polish, English, Spanish, or
         Italian.
       </p>
