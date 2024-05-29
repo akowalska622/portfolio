25bc975c3fe11fd005263480a198d0357f234cd9
Ania Kowalska <a.kowalska622@gmail.com>
2024-05-29 12:23:30 +0200

    update copy

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,13 +1,13 @@
-import classes from "./Footer.module.scss";
+import classes from './Footer.module.scss';
 
 export const Footer = () => {
   return (
     <div className={classes.footer}>
       <div className={classes.footerWrapper}>
-        Created with ❤️ by{" "}
-        <span class={classes.footerHover}>Ania Kowalska</span> in 2021.
-        Copyright &copy; by Ania Kowalska. Always open to job/freelance
-        propositions.
+        Created with ❤️ by{' '}
+        <span class={classes.footerHover}>Ania Kowalska</span> in 2021 -{' '}
+        {new Date().getFullYear()}. Copyright &copy; by Ania Kowalska. Always
+        open to job/freelance propositions.
       </div>
     </div>
   );
